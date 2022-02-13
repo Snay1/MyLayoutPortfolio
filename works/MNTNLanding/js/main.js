9976a3368b53fdbe3e4e89c5d0808cc00a96a839
Snay1 <89585275+Snay1@users.noreply.github.com>
2022-02-13 17:28:45 +0300

    Add files via upload

---
+++ works/MNTNLanding/js/main.js
@@ -0,0 +1,15 @@
+﻿burger_open = document.getElementsByClassName(".burger__open");
+burger_close = document.getElementsByClassName(".burger__close");
+burger = document.getElementsByClassName(".header__burger");
+actualDisplay = getComputedStyle(added_item_button).display;
+
+
+
+document.getElementsByClassName('.burger__button').onclick = function() {
+    alert('click');
+    if (actualDisplay == 'none') {
+        burger.style.display = 'block';
+      } else {
+        burger.innerText += '.';
+      }
+  }
